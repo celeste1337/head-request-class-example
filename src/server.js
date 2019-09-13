f60dc8e80de486d9917b18832c88e1cd1e79b047
Celeste Yost <17mcyost@gmail.com>
2019-09-13 14:54:28 -0400

    check github skinny

--- src/server.js
+++ src/server.js
@@ -5,10 +5,38 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = { // urls routed in here, go to here see if route exists
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+
+};
+
+// sends url to handlers
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  console.dir(request.method);
+  console.dir(parsedUrl.pathname);
 
+  if (!urlStruct[request.method]) {
+    urlStruct.HEAD.notFound(request, response);
+  }
+
+  // returns entire object w the 2 routes in it, index into it w the pathname
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else { // doesnt check for post, only get
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
